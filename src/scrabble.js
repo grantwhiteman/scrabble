e43445768bd05bc7b04d36e455f16b592f36e23f
Grant Whiteman <grantwhiteman95@gmail.com>
2021-06-24 16:02:30 +0100

    added scrabble challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,47 @@
-class Scrabble {
-  // Write your implementation here
 
+
+
+class Scrabble {
+  constructor(word) {
+      this.word = word;
+  }
+  score() {
+    let score = 0;
+
+    if (this.checkValidity()) {
+      return score
+    };
+
+    this.word = this.word.toUpperCase();
+
+    for (let i = 0; i < this.word.length; i++) { //iterates over chars in word
+      for (let key in dict) { //iterates over keys in dictionary
+        if (dict[key].includes(this.word[i])) { //checks if char is in dict
+          score += Number(key);
+        }
+      }
+    }
+    return score;
+  }
+  checkValidity() { //Checks for spaces/null values
+    if ((this.word === null) || (this.word.includes(' '))) {
+      return true;
+    }
+    return false;
+  }
 }
+
+//create dict of scores
+const dict = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']};
+
+let myWord = new Scrabble('flow ers');
+console.log(myWord.score());
 
 module.exports = Scrabble
